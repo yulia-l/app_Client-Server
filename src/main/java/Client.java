c7f8709c93871be67319dcd96319843e27f5f7be
yulia-l <yulia.lyugunaya@yandex.ru>
2022-10-17 16:34:26 +0300

    V.1

---
+++ src/main/java/Client.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Client {
+    public static void main(String[] args) {
+        String host = "localhost";
+        int port = 8089;
+
+        try (Socket clientSocket = new Socket(host, port);
+             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
+        ) {
+            out.println("Julia");
+            String resp = in.readLine();
+            System.out.println(resp);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Server started!");
+        int port = 8089;
+
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            while (true) {
+                try (Socket clientSocket = serverSocket.accept();
+                     PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                     BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
+                ) {
+                    System.out.println("New connection accepted. Port: " + clientSocket.getPort());
+                    final String name = in.readLine();
+                    out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
+                }
+            }
+        }
+    }
+}
